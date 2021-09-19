17785b2ce4dbe2f5bb3802b55a397f2f54662a06
EdwardHackney <59423555+EdwardHackney@users.noreply.github.com>
2021-09-18 23:57:14 -0400

    Update flappy.py

--- FlappyBird/flappy.py
+++ FlappyBird/flappy.py
@@ -1,142 +1,107 @@
-import pygame, random, time
+import pygame, random
 
 # Window Setup
-windowWidth = 750
-windowHeight = 500
+windowWidth = 800
+windowHeight = 600
 pygame.init()
 win = pygame.display.set_mode((windowWidth, windowHeight))
-pygame.display.set_caption("Flappy Bird!")
-resetting = False
 
-font = pygame.font.SysFont("Calibri (Body)", 50)
+pygame.font.init()
+font = pygame.font.SysFont('Comic Sans MS', 30)
 
-losingText = font.render("You lost", True, (50, 50, 50))
-losingTextRect = losingText.get_rect()
+stickForwardImage = pygame.image.load("Color Wall\FinnStick.png").convert_alpha()
 
-losingTextRect.center = (windowWidth // 2, (windowHeight // 2) - 100)
+colors = [
+    (255, 165, 0),
+    (138,43,226),
+    (255, 0, 0),
+    (127,255,0)
+]
 
-scoreText = font.render("0", True, (50, 50, 50))
-scoreTextRect = scoreText.get_rect()
+changeRectColor = (255,255,255)
 
-scoreTextRect.center = (25, 25)
-
-class Player:
-    def __init__(self, x, y, radius, color):
-        self.x = x
-        self.y = y
-        self.radius = radius
-        self.color = color
-
-        self.yVel = 0.1
+class Stickman:
+    def __init__(self, pos):
+        self.sprite = stickForwardImage
         self.dead = False
         self.score = 0
+        self.pos = pos
+        self.sprite = pygame.transform.scale(self.sprite, (75, 75))
+
+    def draw(self):
+        if not self.dead:
+            win.blit(self.sprite, self.pos)
+
+stickman = Stickman((20,230))
+
+class ColorWall:
+    def __init__(self, color, x):
+        self.x = x
+        self.color = color
+
+    def draw(self):
+        pygame.draw.rect(win, self.color, (self.x, 0, 75, 600))
 
     def update(self):
-        # Movement
-        if not resetting:
-            if self.radius < self.y + self.yVel < windowHeight - self.radius: 
-                self.y += self.yVel
-            else:
-                self.yVel = 0
-            self.yVel += 0.2
-        # Drawing
-        if not self.dead:
-            pygame.draw.circle(win, (255, 175, 0), (int(self.x), int(self.y)), self.radius)
-        else:
-            pygame.draw.circle(win, (0, 0, 0), (int(self.x), int(self.y)), self.radius)
+        if not stickman.dead:
+            self.x -= 3
+            if self.x <= -75:
+                self.x = 800
+                stickman.score += 1
+                print(stickman.score)
+                self.color = random.choice(colors)
+            elif self.x >= 20 and self.x <= 95 and changeRectColor != self.color:
+                stickman.dead = True
 
-player = Player(150, 250, 20, (255, 175, 0))
-
-class Pipe:
-    def __init__(self, x, y, width, height, color):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.color = color
-    def draw(self):
-        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
-    def move(self):
-        if not resetting:
-            self.x -= 1
-
-
-pipePairs = []
-
-def resetPipes():
-    for pipe in pipePairs:
-        if pipe[0].x < -pipe[0].width:
-            pipe[0].x = pipe[1].x = windowWidth
-            pipe[0].height = random.randint(30, 350)
-            pipe[1].y = pipe[0].height + 125
-            pipe[1].height = windowHeight - (pipe[0].height + 125)
-
-def pipeCollision():
-    for pipe in pipePairs:
-        if 110 < pipe[0].x < 170:
-            if player.y < pipe[0].height + 20 or player.y > pipe[1].y - 20:
-                player.dead = True
-
-
-def genPipes():
-    pipePairs.clear()
-    for i in range(5):
-        newPipeTop = Pipe((i + 1) * 150 + 150, 0, 20, random.randint(30, 350), (0, 255, 123))
-        newPipeBottom = Pipe((i + 1) * 150 + 150, newPipeTop.height + 125, 20, windowHeight - (newPipeTop.height + 125), (0, 255, 123))
-        pipePairs.append([newPipeTop, newPipeBottom])
-
-genPipes()
 
 running = True
-timeTilReset = 2000
+
+walls = []
+for i in range(2):
+    walls.append(ColorWall(random.choice(colors), (i+3)*875/2))
+
+changeWallColor = (255, 255, 255)
+scoreDisplay = font.render("Your Score: 0", True, (50, 50, 50))
+scoreDisplayText = str(stickman.score)
+
+
 while running:
+    # Window stuff
     pygame.time.delay(10)
 
     for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                player.yVel = -4.5
-            elif event.key == pygame.K_q:
-                running = False
-        elif event.type == pygame.QUIT:
+        if event.type == pygame.QUIT:
             running = False
 
-    win.fill((103, 146, 160))
+    win.fill((135,206,235))
 
-    player.update()
-    
-    for pipe in pipePairs:
-        pipe[0].draw()
-        pipe[0].move()
-        pipe[1].draw()
-        pipe[1].move()
+    #Drawing
+    stickman.draw()
 
-        if not player.dead and pipe[0].x + 20 == player.x and not resetting:
-            player.score += 1
-            scoreText = font.render(str(player.score), True, (0, 0, 0))
+    #Change player color
+    if not stickman.dead:
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_1]:
+            #Orange
+            changeRectColor = colors[0]
+        elif keys[pygame.K_2]:
+            #Purple
+            changeRectColor = colors[1]
+        elif keys[pygame.K_3]:
+            #Red
+            changeRectColor = colors[2]
+        elif keys[pygame.K_4]:
+            #Chartruese
+            changeRectColor = colors[3]
 
-    resetPipes()
-    pipeCollision()
-
-    if player.dead:
-        win.blit(losingText, losingTextRect)
-        resetting = True
-
-    if resetting:
-        timeTilReset -= 10
-        if timeTilReset <= 0:
-            timeTilReset = 2000
-            player.yVel = 0
-            player.y = 250
-            player.score = 0
-            scoreText = font.render(str(player.score), True, (0, 0, 0))
-            resetting = False
-            player.dead = False
-            genPipes()
-
-
-    win.blit(scoreText, scoreTextRect)
-
+    pygame.draw.rect(win, (changeRectColor), (20, 300, 75, 300))
+    for wall in walls:
+        wall.draw()
+        wall.update()
+    scoreDisplayText = font.render("Your Score: " + str(stickman.score), True, (0, 0, 0))
+    win.blit(scoreDisplayText, (25, 15))
     pygame.display.update()
+
+
 
 pygame.quit()
